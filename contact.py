97e026f4088d93b04ec3c6c11cfe928a4ebeae76
PythonMainer <bhva3R3H96>
2019-06-27 21:38:30 +0300

    добавление класса Contact

---
+++ contact.py
@@ -0,0 +1,11 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, address, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.address = address
+        self.mobile = mobile
+        self.email =email
