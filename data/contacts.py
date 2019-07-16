b2a259cad7e41cd2f871c77361b8f7d963bfaf48
PythonMainer <bhva3R3H96>
2019-07-16 12:52:27 +0300

    Загрузка тест. д. из файлов в формате JSON для контактов

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname", middlename="middlename", lastname="lastname",
+            nickname='Вадик', address="address", homephone="+7950258655", mobilephone="+79015225666",
+            workphone="+780054221555", email='1@mail.ru', secondaryphone="+85",
+            email2='2@mail.ru', email3='3@mail.ru', homepage='www.mypage.ru')
+]
+
