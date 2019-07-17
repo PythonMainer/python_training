be964152b250b52c56573ab4a9e4bab8f48c2551
PythonMainer <bhva3R3H96>
2019-07-17 23:58:33 +0300

    Проверка данных, загружаемые из БД и добавления check_ui, удаление группы

--- fixture/group.py
+++ fixture/group.py
@@ -47,6 +47,15 @@ class GroupHelper:
         self.open_main_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.open_main_page()
+        self.group_cache = None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -54,6 +63,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def count(self):
         wd = self.app.wd
